7df32532407ec165cfdb9aef74c9780a33ea3c78
Leon.G <leon.g@binarywalk.com>
2024-01-31 10:08:34 +0800

    change color scheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.git.git-blame-nvim" }
 }
